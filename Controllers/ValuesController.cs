8df10f24a90f09f8544d4c71910d8c202d9cd7e9
William Kita <william.p.kita@gmail.com>
2018-06-03 00:06:35 -0500

    Make minor change to test deployment

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace squip_dotnet_api.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1!!!", "value2" };
+            return new string[] { "value1!!!", "value2!!!" };
         }
 
         // GET api/values/5
