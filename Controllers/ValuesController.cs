321a303a2d19d58c0b83b35659785f638ab92777
William Kita <william.p.kita@gmail.com>
2018-06-04 21:11:12 -0500

    Set up temporary config

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,20 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace squip_dotnet_api.Controllers
 {
+    public class Squip
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public IEnumerable<string> Tags { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private readonly string PrimaryKey;
+
+        private readonly string EndpointUri;
+        private const string DatabaseName = "SquipDb";
+        private const string CollectionName = "SquipCollection";
+
+        public ValuesController(IConfiguration configuration)
         {
-            return new string[] { "value1!!!", "value2!!!" };
+            PrimaryKey = configuration["CosmosDb:AccessKey"];
+            EndpointUri = configuration["CosmosDb:EndpointUri"];
         }
 
-        // GET api/values/5
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Squip> Get()
+        {
+
+            var documentClient = new DocumentClient(new Uri(EndpointUri), PrimaryKey);
+            // Set some common query options
+            var queryOptions = new FeedOptions { MaxItemCount = -1 };
+
+            // Here we find the Andersen family via its LastName
+            var squips = documentClient.CreateDocumentQuery<Squip>(
+                UriFactory.CreateDocumentCollectionUri(DatabaseName, CollectionName), queryOptions);
+
+            return squips.ToList();
+        }
+
         [HttpGet("{id}")]
         public string Get(int id)
         {
