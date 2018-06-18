211c2c114e257ccda17a73fb6fe56878907b1ed5
William Kita <william.p.kita@gmail.com>
2018-06-18 10:59:26 -0500

    Use scoped lifecycle for squip repository

--- Startup.cs
+++ Startup.cs
@@ -24,7 +24,7 @@ namespace SquipApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<ISquipRepository, CosmosDbSquipRepository>();
+            services.AddScoped<ISquipRepository, CosmosDbSquipRepository>();
             services.AddMvc();
             services.AddCors();
         }
