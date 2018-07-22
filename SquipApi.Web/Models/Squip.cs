768638d5a2b9840411a2ce24a45cda3d1bb98aa8
William Kita <william.p.kita@gmail.com>
2018-07-21 23:38:29 -0500

    Remove AWS deps

--- SquipApi.Web/Models/Squip.cs
+++ SquipApi.Web/Models/Squip.cs
@@ -1,35 +1,15 @@
 using System.Collections.Generic;
-using Amazon.DynamoDBv2.DataModel;
-using Amazon.DynamoDBv2.DocumentModel;
-using Newtonsoft.Json;
 
 namespace SquipApi.Models
 {
-    [DynamoDBTable("squips")]
     public class Squip
     {
-        [DynamoDBHashKey("squipId")]
         public string Id { get; set; }
 
-        [DynamoDBProperty("title")]
         public string Title { get; set; }
 
-        [DynamoDBProperty("body")]
         public string Body { get; set; }
         
-        [DynamoDBProperty("tags",typeof(StringListConverter))]
         public IList<string> Tags { get; set; }
-    }
-    public class StringListConverter : IPropertyConverter
-    {
-        public object FromEntry(DynamoDBEntry entry)
-        {
-            return entry.AsListOfString();
-        }
-
-        public DynamoDBEntry ToEntry(object value)
-        {
-            return value as List<string>;
-        }
     }
 }
--- SquipApi.Web/Repositories/SquipRepository.cs
+++ SquipApi.Web/Repositories/SquipRepository.cs
@@ -2,10 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Amazon;
-using Amazon.DynamoDBv2;
-using Amazon.DynamoDBv2.DataModel;
-using Amazon.DynamoDBv2.Model;
 using Microsoft.Extensions.Configuration;
 using SquipApi.Models;
 
@@ -14,37 +10,33 @@ namespace SquipApi.Repositories
     public class DynamoDbSquipRepository : ISquipRepository
     {
 
-        private readonly AmazonDynamoDBClient _dbClient;
-        private readonly DynamoDBContext _dbContext;
-
         public DynamoDbSquipRepository(IConfiguration configuration)
         {
-            _dbClient = new AmazonDynamoDBClient(configuration["AwsKey"], configuration["AwsToken"], RegionEndpoint.GetBySystemName(configuration["AwsRegion"]));
-            _dbContext = new DynamoDBContext(_dbClient);
+            throw new NotImplementedException();
         }
         public async Task<IEnumerable<Squip>> GetAll()
         {
-            return await _dbContext.ScanAsync<Squip>(new List<ScanCondition>()).GetRemainingAsync();
+            throw new NotImplementedException();
         }
 
         public async Task<Squip> GetById(string id)
         {
-            return await _dbContext.LoadAsync<Squip>(id);
+            throw new NotImplementedException();
         }
 
         public async Task Add(Squip squip)
         {
-            await _dbContext.SaveAsync(squip);
+            throw new NotImplementedException();
         }
 
         public async Task Remove(Squip squip)
         {
-            await _dbContext.DeleteAsync(squip);
+            throw new NotImplementedException();
         }
 
         public async Task Update(Squip squip)
         {
-            await _dbContext.SaveAsync(squip);
+            throw new NotImplementedException();
         }
     }
 }
