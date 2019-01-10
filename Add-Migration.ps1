e8c6c2cc10c76e8b068a0fde168b5722df5f6b25
William Kita <william.p.kita@gmail.com>
2019-01-09 23:20:35 -0600

    Delete all the things

--- Add-Migration.ps1
+++
@@ -1,3 +0,0 @@
-Add-Migration Blah -Project SquipApi.EntityFramework -StartupProject SquipApi.WebApi -Context SquipContext
-Remove-Migration -Project SquipApi.EntityFramework -StartupProject SquipApi.WebApi -Context SquipContext
-Update-Database -Context SquipContext
