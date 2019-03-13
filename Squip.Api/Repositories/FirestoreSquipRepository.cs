e5c91fa3408de5505e4fd71d8dc25970d5968e09
William Kita <william.p.kita@gmail.com>
2019-03-12 18:01:10 -0700

    Get really naive randomness going

--- Squip.Api/Repositories/FirestoreSquipRepository.cs
+++ Squip.Api/Repositories/FirestoreSquipRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,18 +9,25 @@ namespace Squip.Api.Repositories
 {
     public class FirestoreSquipRepository : ISquipRepository
     {
-
+        static Random r = new Random();
         public async Task<SquipDto> GetSquip()
         {
-
             var db = await FirestoreDb.CreateAsync("squip-183202");
-            DocumentReference docRef = db.Document("squips/106jp9WkoLcPUytIUjFi");
+            CollectionReference colRef = db.Collection("squips");
+            QuerySnapshot querySnapshot = await colRef.GetSnapshotAsync();
+            var ids = querySnapshot.Documents.Select(d => d.Id).ToList();
+            var index = r.Next(ids.Count);
+            var randomId = ids[index];
+            DocumentReference docRef = colRef.Document(randomId);
             DocumentSnapshot docSnap = await docRef.GetSnapshotAsync();
             SquipDto squip = null;
             if (docSnap.Exists)
             {
                 var docDict = docSnap.ToDictionary();
-                squip = new SquipDto { Content = docDict["content"].ToString() };
+                if (docDict.ContainsKey("content"))
+                {
+                    squip = new SquipDto { Content = docDict["content"].ToString() };
+                }
             }
             return squip;
         }
