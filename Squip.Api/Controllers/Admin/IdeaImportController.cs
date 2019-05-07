c0abeb1587435b7b07c8119fa0f0731a83b55a21
William Kita <william.p.kita@gmail.com>
2019-05-07 06:44:18 -0700

    Add bulk-import for a very limited schema

---
+++ Squip.Api/Controllers/Admin/IdeaImportController.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Squip.Api.DomainModels;
+using Squip.Api.Repositories;
+using Squip.Api.Services;
+
+namespace Squip.Api.Controllers
+{
+    [Route("api/ideaImport")]
+    [ApiController]
+    [ApiConventionType(typeof(DefaultApiConventions))]
+    // [Authorize]
+
+    public class IdeaImportController : ControllerBase
+    {
+        private readonly ISquipService squipService;
+
+        public IdeaImportController(
+            ISquipService squipService)
+        {
+            this.squipService = squipService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(IEnumerable<dynamic> ideasToImport)
+        {
+            var firstSeveralIdeasAsDynamicJson = ideasToImport.Take(100);
+
+            if (firstSeveralIdeasAsDynamicJson == null)
+            {
+                return BadRequest();
+            }
+
+            var ideas = new Collection<Idea>();
+            foreach (var ideaJson in firstSeveralIdeasAsDynamicJson)
+            {
+                var idea = new Idea
+                {
+                    Content = $"# {ideaJson.title}\n![alt text]({ideaJson.media} \"{ideaJson.title}\")"
+                };
+
+                await squipService.Ideate(idea);
+
+                ideas.Add(idea);
+            }
+
+            return Ok(ideas);
+        }
+    }
+}
