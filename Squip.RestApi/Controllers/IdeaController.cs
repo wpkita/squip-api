86dfd507e150a4b46f1aa8874540b483ad6654b4
William Kita <william.p.kita@gmail.com>
2020-02-15 01:27:29 -0800

    Rename controller

--- Squip.RestApi/Controllers/IdeaController.cs
+++ Squip.RestApi/Controllers/IdeaController.cs
@@ -11,14 +11,14 @@ namespace Squip.RestApi.Controllers
 {
     [ApiController]
     [Route("api/ideas")]
-    public class IdeasController : ControllerBase
+    public class IdeaController : ControllerBase
     {
-        private readonly IRepository<Idea> _ideasRepository;
+        private readonly IRepository<Idea> _ideaRepository;
         private readonly IMapper _mapper;
 
-        public IdeasController(IRepository<Idea> ideasRepository, IMapper mapper)
+        public IdeaController(IRepository<Idea> ideaRepository, IMapper mapper)
         {
-            _ideasRepository = ideasRepository;
+            _ideaRepository = ideaRepository;
             _mapper = mapper;
         }
 
@@ -26,7 +26,7 @@ namespace Squip.RestApi.Controllers
         [HttpHead]
         public async Task<IEnumerable<IdeaDto>> GetIdeas()
         {
-            var ideasFromRepo = await _ideasRepository.GetAll();
+            var ideasFromRepo = await _ideaRepository.GetAll();
 
             return _mapper.Map<IEnumerable<IdeaDto>>(ideasFromRepo);
         }
@@ -34,7 +34,7 @@ namespace Squip.RestApi.Controllers
         [HttpGet("{ideaId}", Name = "GetIdea")]
         public async Task<IActionResult> GetIdea(Guid ideaId)
         {
-            var ideaFromRepo = await _ideasRepository.GetById(ideaId.ToString());
+            var ideaFromRepo = await _ideaRepository.GetById(ideaId.ToString());
 
             if (ideaFromRepo == null)
             {
@@ -48,7 +48,7 @@ namespace Squip.RestApi.Controllers
         public async Task<ActionResult<IdeaDto>> CreateIdea(IdeaForCreationDto idea)
         {
             var ideaEntity = _mapper.Map<Idea>(idea);
-            await _ideasRepository.Create(ideaEntity);
+            await _ideaRepository.Create(ideaEntity);
 
             var ideaToReturn = _mapper.Map<IdeaDto>(ideaEntity);
             return CreatedAtRoute("GetIdea", new {ideaId = ideaToReturn.Id}, ideaToReturn);
@@ -57,12 +57,12 @@ namespace Squip.RestApi.Controllers
         [HttpDelete("{ideaId}")]
         public async Task<IActionResult> DeleteIdea(Guid ideaId)
         {
-            if (!await _ideasRepository.DoesExistById(ideaId.ToString()))
+            if (!await _ideaRepository.DoesExistById(ideaId.ToString()))
             {
                 return NotFound();
             }
 
-            await _ideasRepository.Archive(ideaId.ToString());
+            await _ideaRepository.Archive(ideaId.ToString());
 
             return NoContent();
         }
