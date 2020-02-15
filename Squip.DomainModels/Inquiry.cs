f322ac1e1f7763ca174ce0f24d8b377d3851ae10
William Kita <william.p.kita@gmail.com>
2020-02-15 01:27:11 -0800

    Remove old things...

--- Squip.DomainModels/Inquiry.cs
+++
@@ -1,7 +0,0 @@
-namespace Squip.Domain
-{
-    public class Inquiry : DomainModelBase
-    {
-        public string UserId { get; set; }
-    }
-}
--- Squip.DomainModels/Reaction.cs
+++
@@ -1,9 +0,0 @@
-namespace Squip.Domain
-{
-    public class Reaction : DomainModelBase
-    {
-        public string UserId { get; set; }
-        public string PresentationId { get; set; }
-        public string ReactionCategory { get; set; }
-    }
-}
