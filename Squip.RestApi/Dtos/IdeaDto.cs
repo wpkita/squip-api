9c81acb216c73b00d2ac024ca97240c9a764b60a
William Kita <william.p.kita@gmail.com>
2020-02-22 22:50:22 -0800

    Remove unused code

--- Squip.RestApi/Dtos/IdeaDto.cs
+++ Squip.RestApi/Dtos/IdeaDto.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using Squip.Domain;
-using Squip.RestApi.Controllers;
 
 namespace Squip.RestApi.Dtos
 {
@@ -22,20 +18,4 @@ namespace Squip.RestApi.Dtos
             CreateMap<IdeaForCreationDto, Idea>();
         }
     }
-
-    //public class AuthorsProfile : Profile
-    //{
-    //    public AuthorsProfile()
-    //    {
-    //        CreateMap<Entities.Author, Models.AuthorDto>()
-    //            .ForMember(
-    //                dest => dest.Name,
-    //                opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
-    //            .ForMember(
-    //                dest => dest.Age,
-    //                opt => opt.MapFrom(src => src.DateOfBirth.GetCurrentAge()));
-
-    //        CreateMap<Models.AuthorForCreationDto, Entities.Author>();
-    //    }
-    //}
 }
