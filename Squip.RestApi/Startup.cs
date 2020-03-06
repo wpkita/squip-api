97e541718302e2a49a959e52b21f9dc64fb9e52b
William Kita <william.p.kita@gmail.com>
2020-03-06 12:57:10 -0800

    Ignore unused reference to Asp.net call

--- Squip.RestApi/Startup.cs
+++ Squip.RestApi/Startup.cs
@@ -35,6 +35,7 @@ namespace Squip.RestApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // ReSharper disable once UnusedMember.Global
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
