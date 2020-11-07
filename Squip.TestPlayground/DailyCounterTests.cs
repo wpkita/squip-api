e668311fc48cb903ef6f5be721931b2d2c016dfb
William Kita <william.p.kita@gmail.com>
2020-11-07 01:21:46 -0600

    Name unit tests in daily counter based on method name.

--- Squip.TestPlayground/DailyCounterTests.cs
+++ Squip.TestPlayground/DailyCounterTests.cs
@@ -6,7 +6,7 @@ namespace Squip.TestPlayground
     public class DailyCounterTests
     {
         [Fact]
-        public void DailyCounter_CurrentlyAt0_JumpsTo1()
+        public void Activate_CurrentlyAt0_JumpsTo1()
         {
             var dailyCounter = new DailyCounter();
             dailyCounter.Activate();
@@ -14,7 +14,7 @@ namespace Squip.TestPlayground
         }
 
         [Fact]
-        public void DailyCounter_CurrentlyAt0_JumpsTo1_ThenJumpsTo2()
+        public void Activate_CurrentlyAt0_JumpsTo1_ThenJumpsTo2()
         {
             var dailyCounter = new DailyCounter();
             dailyCounter.Activate();
