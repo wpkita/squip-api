f4095428ab49b2fa9969c7488c1218767ae53558
William Kita <william.p.kita@gmail.com>
2020-11-07 01:22:14 -0600

    Write a few daily toggle tests.

---
+++ Squip.TestPlayground/DailyToggleTests.cs
@@ -0,0 +1,54 @@
+using FluentAssertions;
+using Xunit;
+
+namespace Squip.TestPlayground
+{
+    public class DailyToggleTests
+    {
+        [Fact]
+        public void Toggle_WasFalse_NowIsTrue()
+        {
+            var dailyToggle = new DailyToggle(false);
+
+            dailyToggle.Toggle();
+
+            dailyToggle.IsOn.Should().Be(true);
+        }
+
+        [Fact]
+        public void Toggle_WasTrue_NowIsFalse()
+        {
+            var dailyToggle = new DailyToggle(true);
+
+            dailyToggle.Toggle();
+
+            dailyToggle.IsOn.Should().Be(false);
+        }
+
+        [Fact]
+        public void Toggle_WasFalseAndToggledTwice_StillIsFalse()
+        {
+            var dailyToggle = new DailyToggle(false);
+
+            dailyToggle.Toggle();
+            dailyToggle.Toggle();
+
+            dailyToggle.IsOn.Should().Be(false);
+        }
+    }
+
+    public class DailyToggle
+    {
+        public DailyToggle(bool isOn)
+        {
+            IsOn = isOn;
+        }
+
+        public void Toggle()
+        {
+            IsOn = !IsOn;
+        }
+
+        public bool IsOn { get; private set; }
+    }
+}
