0bf0a8452203c1b82517d2ab0ea6a1b33413edb6
William Kita <william.p.kita@gmail.com>
2020-11-07 00:27:00 -0600

    Create first tests for daily counter

---
+++ Squip.TestPlayground/DailyCounterTests.cs
@@ -0,0 +1,41 @@
+using FluentAssertions;
+using Xunit;
+
+namespace Squip.TestPlayground
+{
+    public class DailyCounterTests
+    {
+        [Fact]
+        public void DailyCounter_CurrentlyAt0_JumpsTo1()
+        {
+            var dailyCounter = new DailyCounter();
+            dailyCounter.Activate();
+            dailyCounter.Value.Should().Be(1);
+        }
+
+        [Fact]
+        public void DailyCounter_CurrentlyAt0_JumpsTo1_ThenJumpsTo2()
+        {
+            var dailyCounter = new DailyCounter();
+            dailyCounter.Activate();
+            dailyCounter.Value.Should().Be(1);
+            dailyCounter.Activate();
+            dailyCounter.Value.Should().Be(2);
+        }
+    }
+
+    public class DailyCounter
+    {
+        public DailyCounter(int value = 0)
+        {
+            Value = value;
+        }
+
+        public int Value { get; private set; }
+
+        public void Activate()
+        {
+            Value += 1;
+        }
+    }
+}
