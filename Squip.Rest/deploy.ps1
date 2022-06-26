aa85e69b8d5e6f0a7e5f9ab49a3baff0cc249ef9
William Kita <wpkita@outlook.com>
2022-06-26 15:59:33 -0500

    Deploy Cloud Run revision in deploy script

--- Squip.Rest/deploy.ps1
+++ Squip.Rest/deploy.ps1
@@ -1,3 +1,5 @@
 docker build -t squip-api .
 docker tag squip-api us-central1-docker.pkg.dev/squip-project/squip-repository/squip-api
 docker push us-central1-docker.pkg.dev/squip-project/squip-repository/squip-api
+gcloud config set run/region us-central1
+gcloud run deploy squip-api --image=us-central1-docker.pkg.dev/squip-project/squip-repository/squip-api
