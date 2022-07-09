60dc45a3cf237e4c720119fffa22ced096febc6c
William Kita <wpkita@outlook.com>
2022-07-09 17:58:38 -0500

    Get Elasticsearch logging to work from local

--- Squip.Rest/Program.cs
+++ Squip.Rest/Program.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Reflection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
+using Serilog.Sinks.Elasticsearch;
 
 namespace Squip.Rest
 {
@@ -10,12 +13,7 @@ namespace Squip.Rest
     {
         public static int Main(string[] args)
         {
-            Log.Logger = new LoggerConfiguration().MinimumLevel
-                .Override("Microsoft", LogEventLevel.Information)
-                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
-                .Enrich.FromLogContext()
-                .WriteTo.Console()
-                .CreateLogger();
+            ConfigureLogging();
 
             try
             {
@@ -32,6 +30,33 @@ namespace Squip.Rest
             {
                 Log.CloseAndFlush();
             }
+        }
+
+        private static void ConfigureLogging()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddUserSecrets(Assembly.GetExecutingAssembly())
+                .Build();
+
+            Log.Logger = new LoggerConfiguration().MinimumLevel
+                .Override("Microsoft", LogEventLevel.Information)
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.Elasticsearch(
+                    new ElasticsearchSinkOptions(new Uri(configuration["Elasticsearch:Uri"]))
+                    {
+                        TypeName = null,
+                        ModifyConnectionSettings = x =>
+                            x.BasicAuthentication(
+                                configuration["Elasticsearch:Username"],
+                                configuration["Elasticsearch:Password"]
+                            ),
+                        IndexFormat =
+                            $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower()}-{"local".Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
+                    }
+                )
+                .CreateLogger();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
