1b3bd7f839817375f05d7d7aa1c05ed57407088d
William Kita <wpkita@outlook.com>
2022-12-19 16:20:55 -0600

    Route traffic to latest deploy.

--- Squip.Rest/deploy.sh
+++ Squip.Rest/deploy.sh
@@ -3,3 +3,4 @@ docker tag squip-api us-central1-docker.pkg.dev/squip-project/squip-repository/s
 docker push us-central1-docker.pkg.dev/squip-project/squip-repository/squip-api
 gcloud config set run/region us-central1
 gcloud run deploy squip-api --image=us-central1-docker.pkg.dev/squip-project/squip-repository/squip-api
+gcloud run services update-traffic squip-api --to-latest
