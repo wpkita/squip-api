2b44eaf13395f5a8daa4111baab969825335c1bf
William Kita <wpkita@outlook.com>
2023-07-31 03:45:39 -0500

    Write several queries for patterns between moods and habits.

---
+++ tools/aggregate/moods-and-habits-correlation.sql
@@ -0,0 +1,62 @@
+select corr("Happiness", "habittotal")          happiness,
+       corr("Hopefulness", "habittotal")        hopefulness,
+       corr("Sadness", "habittotal")            sadness,
+       corr("Pain", "habittotal")               pain,
+       corr("Guilt/Shame", "habittotal")        guilt_shame,
+       corr("Fear/Anxiety", "habittotal")       fear_anxiety,
+       corr("Numb/Empty", "habittotal")         numb_empty,
+       corr("Irritable/Agitated", "habittotal") irritable_agitated,
+       corr("Anger", "habittotal")              anger
+from (select mods.*,
+             habs.habittotal
+      from (select date(hib.instant_created_at)     habitDate,
+                   count(hib.instant_created_at) as HabitTotal
+            from hibits as hib
+                     inner join habits as hab on hab.id = hib.habit_id
+                and hab.is_archived = false
+                and date(hib.instant_created_at) <> '2022-08-27'
+            group by date(hib.instant_created_at)) habs
+               inner join (select date(me.instant_created_at) moodDate,
+                                  avg(me.magnitude) filter (
+                                      where
+                                      mo.name = 'Happiness'
+                                      ) as                    "Happiness",
+                                  avg(me.magnitude) filter (
+                                      where
+                                      mo.name = 'Hopefulness'
+                                      ) as                    "Hopefulness",
+                                  avg(me.magnitude) filter (
+                                      where
+                                      mo.name = 'Sadness'
+                                      ) as                    "Sadness",
+                                  avg(me.magnitude) filter (
+                                      where
+                                      mo.name = 'Pain'
+                                      ) as                    "Pain",
+                                  avg(me.magnitude) filter (
+                                      where
+                                      mo.name = 'Guilt/Shame'
+                                      ) as                    "Guilt/Shame",
+                                  avg(me.magnitude) filter (
+                                      where
+                                      mo.name = 'Fear/Anxiety'
+                                      ) as                    "Fear/Anxiety",
+                                  avg(me.magnitude) filter (
+                                      where
+                                      mo.name = 'Numb/Empty'
+                                      ) as                    "Numb/Empty",
+                                  avg(me.magnitude) filter (
+                                      where
+                                      mo.name = 'Irritable/Agitated'
+                                      ) as                    "Irritable/Agitated",
+                                  avg(me.magnitude) filter (
+                                      where
+                                      mo.name = 'Anger'
+                                      ) as                    "Anger"
+                           from mood_entries as me
+                                    inner join moods as mo on mo.id = me.mood_id
+                               and mo.user_id = '55ce7706-7cac-47d0-90ca-1273d28bb1b6'
+                               and mo.is_archived = false
+                               and date(me.instant_created_at) <> '2022-08-27'
+                           group by date(me.instant_created_at)) mods on habs.habitDate = mods.moodDate
+      order by mods.moodDate) dailyTotals
---
+++ tools/aggregate/moods-and-habits-even-empty.sql
@@ -0,0 +1,62 @@
+select *
+from (select date(d),
+             count(hib.instant_created_at) as HabitTotal
+      from generate_series(
+                   timestamp without time zone '2023-01-01',
+                   timestamp without time zone '2023-07-31',
+                   '1 day'
+               ) as gs(d)
+               left outer join hibits as hib
+               inner join habits as hab on hab.id = hib.habit_id on hib.instant_created_at :: Date = date(d)
+          and hab.is_archived = false
+          and date(hib.instant_created_at) <> '2022-08-27'
+      group by date(d)) habs
+         inner join (select date(d),
+                            avg(me.magnitude) filter (
+                                where
+                                mo.name = 'Happiness'
+                                ) as "Happiness",
+                            avg(me.magnitude) filter (
+                                where
+                                mo.name = 'Hopefulness'
+                                ) as "Hopefulness",
+                            avg(me.magnitude) filter (
+                                where
+                                mo.name = 'Sadness'
+                                ) as "Sadness",
+                            avg(me.magnitude) filter (
+                                where
+                                mo.name = 'Pain'
+                                ) as "Pain",
+                            avg(me.magnitude) filter (
+                                where
+                                mo.name = 'Guilt/Shame'
+                                ) as "Guilt/Shame",
+                            avg(me.magnitude) filter (
+                                where
+                                mo.name = 'Fear/Anxiety'
+                                ) as "Fear/Anxiety",
+                            avg(me.magnitude) filter (
+                                where
+                                mo.name = 'Numb/Empty'
+                                ) as "Numb/Empty",
+                            avg(me.magnitude) filter (
+                                where
+                                mo.name = 'Irritable/Agitated'
+                                ) as "Irritable/Agitated",
+                            avg(me.magnitude) filter (
+                                where
+                                mo.name = 'Anger'
+                                ) as "Anger"
+                     from generate_series(
+                                  timestamp without time zone '2023-01-01',
+                                  timestamp without time zone '2023-07-31',
+                                  '1 day'
+                              ) as gs(d)
+                              left outer join mood_entries as me
+                              inner join moods as mo on mo.id = me.mood_id
+                         and mo.user_id = '55ce7706-7cac-47d0-90ca-1273d28bb1b6'
+                                         on me.instant_created_at :: Date = date(d)
+                                             and mo.is_archived = false
+                                             and date(me.instant_created_at) <> '2022-08-27'
+                     group by date(d)) mods on habs.date = mods.date
